3b6b37f00128bc7cda6329a65152f4d5832a1ea3
Sifeng Chen <sifengchen2002@gmail.com>
2023-05-12 02:18:27 -0400

    final??

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Sidebar from "../components/sidebar";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+        <Sidebar />
+        <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
+          {children}
+          {/* <Analytics /> */}
+        </main>
+      </body>{" "}
     </html>
   );
 }

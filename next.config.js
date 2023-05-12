68f79b8d98dfd4a064777b3a741d396b5fb40f14
Sifeng Chen <sifengchen2002@gmail.com>
2023-05-12 02:55:35 -0400

    export in config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   experimental: {
     appDir: true,
   },
   async rewrites() {
     return [
       {
-        source: '/:any*',
-        destination: '/',
+        source: "/:any*",
+        destination: "/",
       },
     ];
   },
   theme: {
     extend: {
       grayscale: {
-        50: '50%',
-      }
-    }
+        50: "50%",
+      },
+    },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

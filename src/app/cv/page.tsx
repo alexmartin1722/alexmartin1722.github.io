804a3c804727da48837936ca76f68f039d1940f4
Sifeng Chen <sifengchen2002@gmail.com>
2023-05-12 02:33:42 -0400

    exported

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-function Cv() {
+export default function Cv() {
   return (
     <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
       <h1> This is CV</h1>
@@ -8,4 +8,3 @@ function Cv() {
   );
 }
 
-export default Cv;
--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 
-function Publications() {
+export default function Publications() {
   return (
     <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
       <h1> This is publications</h1>
     </main>
   );
 }
-
-export default Publications;

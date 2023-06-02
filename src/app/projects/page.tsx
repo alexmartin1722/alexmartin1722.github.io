c84138ec6847d69e6ab197c4545462592ee56e5f
Sifeng Chen <sifengchen2002@gmail.com>
2023-06-02 17:29:57 -0400

    project page added

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
+      <h1> This is projects</h1>
+    </main>
+  );
+}

cede69f71bc871699ab871931d99201703463ed9
Sifeng Chen <sifengchen2002@gmail.com>
2023-06-06 03:13:39 -0400

    nav bar text focus updated

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
+// import { useRouter } from "next/navigation";
 // import { LayoutGroup, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const navItems = {
   "/": {
@@ -13,42 +14,65 @@ const navItems = {
   "/publications": {
     name: "Publications",
   },
+  "/projects": {
+    name: "Projects",
+  },
 };
 
 export default function NavBar() {
-  let pathname = usePathname() || "/";
-  if (pathname.includes("/blog/")) {
-    pathname = "/blog";
+  const pathname = usePathname();
+
+  function isActive(yo: String) {
+    if (pathname === yo) {
+      return "";
+    } else {
+      return "text-neutral-500";
+    }
   }
+
   return (
     <aside className="md:w-[150px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif">
       <nav className="flex overflow-hidden flex-row md:flex-col items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative">
         <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
-          {Object.entries(navItems).map(([path, { name }]) => {
-            const isActive = path === pathname;
-            return (
-              <Link
-                key={path}
-                href={path}
-                className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-[5px] px-[10px] text-neutral-500"
-              >
-                <span className="relative py-[5px] px-[10px]">
-                  {name}
-                  {/* {path === pathname ? (
-                    <motion.div
-                      className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
-                      layoutId="sidebar"
-                      transition={{
-                        type: "spring",
-                        stiffness: 350,
-                        damping: 30,
-                      }}
-                    />
-                  ) : null} */}
-                </span>
-              </Link>
-            );
-          })}
+          <Link
+            className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-[5px] px-[10px] ${isActive(
+              "/"
+            )}`}
+            href="/"
+          >
+            {" "}
+            About
+          </Link>
+
+          <Link
+            className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-[5px] px-[10px] ${isActive(
+              "/publications"
+            )}`}
+            href="/publications"
+          >
+            {" "}
+            Publications
+          </Link>
+
+          <Link
+            className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-[5px] px-[10px] ${isActive(
+              "/projects"
+            )}`}
+            href="/projects"
+          >
+            {" "}
+            Projects
+          </Link>
+
+          <Link
+            className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-[5px] px-[10px] ${isActive(
+              "/cv"
+            )}`}
+            href="/cv"
+          >
+            {" "}
+            CV
+          </Link>
         </div>
       </nav>
     </aside>

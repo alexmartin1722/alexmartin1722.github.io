a03f9fc39061d7831ae2771c2085a8334330c8d1
Alexander Martin <amart50@u.rochester.edu>
2023-06-17 13:09:49 -0400

    set targets of paper links

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -6,13 +6,13 @@ export default function Publications() {
       <h1>All</h1>
       <ul>
         <h1>2023</h1>
-        <li> <a href="https://doi.org/10.3390/ijerph20054539">A New Interpretation of Relative Importance on An Analysis of Per and Polyfluorinated Alkyl Substances (PFAS) Exposures on Bone Mineral Density</a> </li>
+        <li> <a href="https://doi.org/10.3390/ijerph20054539" target="_blank">A New Interpretation of Relative Importance on An Analysis of Per and Polyfluorinated Alkyl Substances (PFAS) Exposures on Bone Mineral Density</a> </li>
         <h1>Unpublished</h1>
-        <li> <a href="https://arxiv.org/">Jurassic World Remake: Bringing Ancient Fossils Back to Life via Zero-Shot Long Image-to-Image Translation</a> </li>
-        <li> <a href="https://arxiv.org/">MegaWika: Millions of reports and their sources across 50 diverse languages</a></li>
-        <li> <a href="https://arxiv.org/">Probabilities Associated with a Dynamic Game</a></li>
-        <li> <a href="https://arxiv.org/">Regression on subspaces: A tool for mixtures with applications to the effects of per-and polyfluoroalkyl substances on risk of major osteoporotic fracture</a> </li>
-        <li> <a href="https://arxiv.org/">Jurassic World Remake: Bringing Ancient Fossils Back to Life via Zero-Shot Long Image-to-Image Translation</a> </li>
+        <li> <a href="https://arxiv.org/" target="_blank">Jurassic World Remake: Bringing Ancient Fossils Back to Life via Zero-Shot Long Image-to-Image Translation</a> </li>
+        <li> <a href="https://arxiv.org/" target="_blank">MegaWika: Millions of reports and their sources across 50 diverse languages</a></li>
+        <li> <a href="https://arxiv.org/" target="_blank">Probabilities Associated with a Dynamic Game</a></li>
+        <li> <a href="https://arxiv.org/" target="_blank">Regression on subspaces: A tool for mixtures with applications to the effects of per-and polyfluoroalkyl substances on risk of major osteoporotic fracture</a> </li>
+        <li> <a href="https://arxiv.org/" target="_blank">Jurassic World Remake: Bringing Ancient Fossils Back to Life via Zero-Shot Long Image-to-Image Translation</a> </li>
       </ul>
     </main>
   );

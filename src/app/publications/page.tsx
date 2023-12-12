74f36539d9a191ccae0229d52d047b8443e24cd9
Sifeng Chen <sifengchen2002@gmail.com>
2023-12-12 16:22:46 -0500

    key added

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -8,8 +8,8 @@ export default function Publications() {
     <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
       <h1 className="text-2xl mb-[15px] font-semibold"> Publications </h1>
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
-        {publication_list.map((paper) => (
-          <ListItem item={paper} />
+        {publication_list.map((paper, key) => (
+          <ListItem key={key} item={paper} />
         ))}
       </ol>
     </main>

70c0605fc4d3db7e923295618bbf33afe7554b05
Sifeng Chen <sifengchen2002@gmail.com>
2023-12-12 16:28:07 -0500

    url added in json

--- src/app/publications/Components/ListItem.tsx
+++ src/app/publications/Components/ListItem.tsx
@@ -6,6 +6,7 @@ interface ListItemProps {
     title: string;
     conference: string;
     description: string;
+    url: string;
     // ... add more properties as needed
   };
 }
@@ -24,30 +25,31 @@ const ListItem: React.FC<ListItemProps> = ({ item }) => {
       <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
         {item.title}
       </h3>
-      {}
       <ReadMoreComponent>{item.description}</ReadMoreComponent>
-      <a
-        href="#"
-        target="_blank"
-        className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-      >
-        Paper
-        <svg
-          className="w-3 h-3 ml-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
+      {item.url && (
+        <a
+          href={item.url}
+          target="_blank"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
         >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </a>
+          Paper
+          <svg
+            className="w-3 h-3 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      )}
     </li>
   );
 };

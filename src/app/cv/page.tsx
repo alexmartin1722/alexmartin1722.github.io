7c9853d634bd2c1932b61a693bdbfb9171145cad
Sifeng Chen <sifengchen2002@gmail.com>
2023-12-14 13:54:18 -0500

    yolo

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,7 +1,7 @@
 export default function Cv() {
   return (
     <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
-      <h1> This is CV</h1>
+      <h1>  CV</h1>
     </main>
   );
 }
